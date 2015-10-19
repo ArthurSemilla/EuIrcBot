bdc489bff121690b34766682e185bbab1edf33a0
Sam Pilla <sjpn96@mst.edu>
2015-10-19 13:05:03 -0500

    added drink module

--- modules/drink/drink.js
+++ modules/drink/drink.js
@@ -1,17 +1,40 @@
 exports.command = "drink";
 
-var options;
+base = [
+"Root Beer",
+"Coke",
+"Dr. Pepper",
+"Sprite",
+"Powerade",
+"Lemonade",
+"Fruit Punch",
+"Limeade"
+]
 
-module.exports.init = function(b) {
+mods = [
+"Cherry",
+"Strawberry",
+"Cranberry"
+]
+
+module.exports.init = function(b)
+{
   bot = b;
-  bot.readDataFile("options.json", function(err, data) {
-    if(err) {
+  bot.readDataFile("options.json", function(err, data)
+  {
+    if(err) 
+    {
       console.log("Error opening options file.");
       options = {};
-    } else {
-      try {
+    }
+    else
+    {
+      try
+      {
         options = JSON.parse(data);
-      } catch(ex) {
+      }
+      catch(ex)
+      {
         console.log("Corrupted options.json file. Defaulting to blank...");
         options = {};
       }
@@ -19,27 +42,22 @@ module.exports.init = function(b) {
   });
 };
 
-function normal_random(){
-  var pos = [ Math.random(), Math.random() ];
-  while ( Math.sin( pos[0] * Math.PI ) > pos[1] ){
-    pos = [ Math.random(), Math.random() ];
-  }
-  return pos[0];
-};
-
-rand = function(arr) {
+rand = function(arr)
+{
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-insult = function() {
-  return rand(adj) + ' ' + rand(ing) + ' ' + rand(noun) + rand(suf);
+drinkgen = function()
+{
+  var pre = (rand % 2 ? "Diet" : "");
+  return pre + ' ' + rand(base) + ' ' + rand(noun);
 }
 
-exports.run = function(r, p, reply) {
-  var diss = '';
-  if(p.length > 0) {
-    diss = diss + p.join(', ') + ': ';
+exports.run = function(r, p, reply)
+{
+  var drink = '';
+  if(p.length > 0)
+  {
+    drink = drink + p.join(', ') + ': ';
   }
-
-  reply(diss + 'you ' + insult());
 }
